71120e3282533e1748ce25cdd44f6f01ddaee6c6
Omikhleia <didier.willis@gmail.com>
2021-10-03 21:23:53 +0200

    feat(packages): Enhancements to no., nos. and siecle support in abbr package

--- packages/abbr.lua
+++ packages/abbr.lua
@@ -18,7 +18,7 @@ SILE.registerCommand("abbr:nbsp", function (options, _)
     local widthsp = enlargement.."spc plus "..stretch.."spc minus "..shrink.."spc"
     SILE.call("kern", { width = widthsp })
   end
-end, "Inserts a non-breakable space (by default shrinkable and stretchable, unless fixed=true)")
+end, "Inserts a non-breakable inter-word space (by default shrinkable and stretchable, unless fixed=true)")
 
 SILE.registerCommand("abbr:no:fr", function (_, content)
   SILE.typesetter:typeset("n")
@@ -33,16 +33,38 @@ SILE.registerCommand("abbr:no:en", function (_, content)
   SILE.process(content)
 end, "Formats an English number, as in no. 5")
 
-
 SILE.registerCommand("abbr:no", function (_, content)
   local lang = SILE.settings.get("document.language")
   if SILE.Commands["abbr:no:"..lang] then
-    SILE.call("abbr:no:"..lang)
+    SILE.call("abbr:no:"..lang, {}, content)
   else
     SU.warn("Language not supported for abbr:no, fallback to English")
-    SILE.call("abbr:no:en")
+    SILE.call("abbr:no:en", {}, content)
   end
 end, "Formats an number, as in no. 5, but depending on language")
+
+SILE.registerCommand("abbr:nos:fr", function (_, content)
+  SILE.typesetter:typeset("n")
+  SILE.call("style:superscript", {}, { "os" })
+  SILE.call("abbr:nbsp")
+  SILE.process(content)
+end, "Formats French numbers (pluralized)")
+
+SILE.registerCommand("abbr:nos:en", function (_, content)
+  SILE.typesetter:typeset("nos.")
+  SILE.call("abbr:nbsp")
+  SILE.process(content)
+end, "Formats English numbers (pluralized)")
+
+SILE.registerCommand("abbr:nos", function (_, content)
+  local lang = SILE.settings.get("document.language")
+  if SILE.Commands["abbr:nos:"..lang] then
+    SILE.call("abbr:nos:"..lang, {}, content)
+  else
+    SU.warn("Language not supported for abbr:nos, fallback to English")
+    SILE.call("abbr:nos:en", {}, content)
+  end
+end, "Formats numbers, as in nos. 5-6, but depending on language")
 
 SILE.registerCommand("abbr:vol", function (_, content)
   SILE.typesetter:typeset("vol.")
@@ -74,10 +96,14 @@ end, "Formats a page reference, as in p. 153, possibly followed by an option-dep
 SILE.registerCommand("abbr:siecle", function (_, content)
   local century = (type(content[1]) == "string") and content[1]
     or SU.error("Expected a string for abbr:siecle")
-  -- experimental because here we expect the user to input a lowercase roman
-  -- number. But shouldn't we detect the input (uppercase roman, bare number, etc.)
-  -- and fix it appropriately, e.g. \abbr:siecle{iv}, \abbr:siecle{IV} or
-  -- \abbr:siecle{4} would have the same output?
+
+  if tonumber(century) ~= nil then
+    century = SILE.formatCounter({ display = "roman", value = century })
+  elseif century:match("^[IVX]+$") ~= nil then
+    century = string.lower(century)
+  elseif century:match("^[ivx]+$") == nil then
+    SU.error("Not a valid century '"..century.. "' in abbr:siecle")
+  end
   SILE.call("font", { features = "+smcp" }, function ()
     SILE.typesetter:typeset(century)
   end)
@@ -86,7 +112,7 @@ SILE.registerCommand("abbr:siecle", function (_, content)
   else
     SILE.call("style:superscript", {}, { "e" })
   end
-end, "Formats an French century (siècle) as in IVe (but properly typeset) - experimental")
+end, "Formats an French century (siècle) as in IVe (but properly typeset)")
 
 return {
   documentation = [[\begin{document}
@@ -94,13 +120,20 @@ return {
     This package defines a few shorthands and abbreviations that its author often
     uses in articles or book chapters.
 
-    The \code{\\abbr:nbsp} command inserts a non-breakable inter-word space.
+    The \code{\\abbr:nbsp} command inserts a non-breakable space.
     It is stretchable and shrinkable as a normal inter-word space by default,
     unless setting the \code{fixed} option to true.
 
     The \code{\\abbr:no:fr} and \code{\\abbr:no:en} commands prepend a
     correctly typeset issue number, for French and English respectively,
     that is \abbr:no:fr{5} and \abbr:no:en{5}.
+
+    The \code{\\abbr:nos:fr} and \code{\\abbr:nos:en} commands are the same
+    as the previous commands, but for the plural, as in
+    \abbr:nos:fr{5–6} and \abbr:nos:en{5–6}.
+
+    The \code{\\abbr:no} and \code{\\abbr:nos} invoke the appropriate
+    command depending on the current language, \abbr:nos{12, 13}.
 
     The \code{\\abbr:vol} acts similarly for volume references, that
     is \abbr:vol{4}, just ensuring the space in between is unbreakable.
@@ -115,6 +148,6 @@ return {
 
     The \code{\\abbr:siecle} command formats a century according to
     the French typographical rules, as in \abbr:siecle{i} or
-    \abbr:siecle{iv}.
+    \abbr:siecle{iv}, \abbr:siecle{15} and \abbr:siecle{XIX}.
   \end{document}]]
 }
