0622815b78244cfcfb9766e3b49cf48a1e8af087
Omikhleia <didier.willis@gmail.com>
2022-06-07 20:50:13 +0200

    feat(packages): Add some missing USFX tags for the Segond Bible

--- packages/experimental/usfx.lua
+++ packages/experimental/usfx.lua
@@ -8,10 +8,25 @@ SILE.require("packages/xmltricks")
 
 SILE.call("xmltricks:ignore", {}, { "languageCode rem id ide toc table" })
 -- languageCode: Three-letter Ethnologue code or two-letter ISO language code, optionally followed
--- by a dialect indicator.
+--   by a dialect indicator.
 -- rem: Remark or comment, not for publication.
+-- ide: Encoding of the corresponding USFM file. (Encoding of this XML file is given in the XML header.)
+-- id: From the \id marker line. The first three characters of the contents of this element MUST
+--   be the three-letter code for this book of the Bible (or FRT for front matter and BAK for back matter).
+--   N.B.in the Segond Bible, at least, the id attribute on the <book> tag contains the Bible book code.
 
-SILE.call("xmltricks:passthru", {}, { "h s xo xt q it ord b qs cs wj" })
+SILE.call("xmltricks:passthru", {}, { "h b qs it cs wj" })
+-- qs: Special marking for "Selah." in the target language. This may cause right alignment of this word or phrase
+--   in an otherwise left-aligned or fully justified paragraph, or may have no effect.
+-- it: talic; not recommended for use. N.B. In the Segond Bible, marks some words or quotations in the comments,
+--   interpretations, introductory text, etc. (e.g. in <p sfm="id">...)
+-- cs: character style. N.B. In the Segond Bible, aways seem to be in the form of <cs sfm="ior">1:3–2:4</cs>
+--  with a range of verses.
+-- wj: Words of Jesus. N.B. In the Segond Bible, also applies to words of God apparently...
+
+-- FIXME
+-- h: Short book name for a running header, like "Matthew".
+--  N.B. Should we use it, or expand the book id from the other provided XML files (e.g. BookNames.xml)?
 
 SILE.registerCommand("book", function (options, content)
   -- This element contains one book of the Bible or optionally some front or back matter.
@@ -86,6 +101,45 @@ SILE.registerCommand("p", function (options, content)
   end
 end)
 
+SILE.registerCommand("q", function (options, content)
+  -- FIXME for now same as p
+  if not options.sfm then
+    if chapStart then
+      chapStart = false
+      -- Problem here: we will want to use a "dropped capital" layout for the chapter number
+      -- but some verses are one line long only. Heavy workaround: parbox the text in a typesetter
+      -- state, steal that box, check its height and act depending on it...
+      -- SILE.call("noindent")
+      SILE.typesetter:pushState()
+      local p = SILE.call("parbox", { width = "100%fw", valign="bottom", border="1pt" }, function()
+            SILE.call("dropcap", { lines = 2, family = "Libertinus Serif",
+              style = "italic", color = "#58101c", join = false },
+              { chap .. " " })
+            SILE.process(content)
+        end)
+      table.remove(SILE.typesetter.state.nodes) -- steal it back
+      SILE.typesetter:popState()
+      local h = p.height + p.depth
+      SILE.typesetter:leaveHmode()
+      
+        SILE.call("dropcap", { lines = 2, family = "Libertinus Serif",
+              style = "italic", color = "#58101c", join = false },
+              { chap .. " " })
+        SILE.process(content)
+        SILE.call("par")
+      
+      if h:tonumber() < SILE.length("2bs"):tonumber() then SILE.call("skip", { height = "1bs" }) end
+    else
+      --SILE.call("color", { color = "blue" }, function()
+      --    SILE.call("textsuperscript", {}, { options.id })
+      -- end)
+      SILE.call("kern", { width = "0.75spc" })
+      SILE.process(content)
+    end
+    SILE.call("par")
+  end
+end)
+
 SILE.registerCommand("c", function (options, content)
   chap = options.id
   chapStart = true
@@ -113,4 +167,22 @@ SILE.registerCommand("x", function (options, content)
   end)
   -- FIXME xt, xo processing with insertions
   -- SILE.process(content)
+  -- This format is a mess of loose structures (e.g. <ve />), we'd need to collect all <x>
+  -- for a given verse, but the latter is not represented by an englobing stucture. This
+  -- format is a mess ^^
+end)
+
+SILE.registerCommand("ord", function (options, content)
+  -- ord: Ordinal number ending; may be superscripted and underlined.
+  -- N.B. In the Segond Bible, used in introductory contents (p sfm=id, etc)
+  SILE.call("textsuperscript", {}, content[0])
+end)
+
+SILE.registerCommand("b", function (options, content)
+  -- b: Blank line between stanzas of poetry; not to be used before or after a section header. 
+  --   This element should be empty. N.B. In the Segond Bible, often used in the Psalms...
+  --   FIXME should perhaps be handled, but sometimes it is between p, sometimes between q, sometimes just
+  --   after a c chapter, and not so consistently... so I am not sure how to treat it generically...
+  -- A skip looks ugly for now...
+  -- SILE.call("skip", { height = "1bs "})
 end)
