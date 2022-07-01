a5432ebc883eab0f524c10e4b3a27eed396c227c
Omikhleia <didier.willis@gmail.com>
2022-07-01 21:29:56 +0200

    refactor: Bargraph refactor (functional but code is still ugly)

--- packages/experimental/charts.lua
+++ packages/experimental/charts.lua
@@ -71,7 +71,9 @@ local function hslToRgb (h, s, l)
 end
 
 local function colorToHsl (color)
-  if color.r then return rgbToHsl(color) end
+  if color.r then
+    return rgbToHsl(color)
+  end
   if color.k then
     -- First convert CMYK to RGB
     local kr = (1 - color.k)
@@ -79,7 +81,7 @@ local function colorToHsl (color)
       r = (1 - color.c) * kr,
       g = (1 - color.m) * kr,
       b = (1 - color.y) * kr,
-  })
+    })
   end
   if color.l then
     -- First convert Grayscale to RGB
@@ -92,7 +94,7 @@ local function colorToHsl (color)
   SU.error("Invalid color specification")
 end
 
--- MEASUREMENT HELPER FUNCTIONS
+--- MEASUREMENT HELPER FUNCTIONS
 
 -- General note on these functions:
 -- We measure all data by constructing hbox'es, so we work in a temporary
@@ -114,8 +116,8 @@ local function measureBiggestCategoryDimens (data)
   SILE.settings.temporarily(function ()
     SILE.settings.set("current.parindent", SILE.length())
     SILE.settings.set("document.parindent", SILE.length())
-    for _, entry in ipairs(data) do
-      local hbox = SILE.call("hbox", {}, { tostring(entry[1]) })
+    for k = 2, #data do
+      local hbox = SILE.call("hbox", {}, { tostring(data[k][1]) })
       if hbox.width > maxWidth then
         maxWidth = hbox.width
       end
@@ -156,21 +158,21 @@ end
 -- Measure value cells, assuming numbers
 -- Returns:
 --  - the max height and width needed,
---  - the max and min values
---  - the max number of columns
+--  - the max and min values,
+--  - the max number of columns.
 local function measureBiggestValueDimens (data)
   local maxWidth = SILE.length()
   local maxHeight = SILE.length()
   local minValue, maxValue
-  local ncols = 0
+  local numberOfCols = 0
 
   SILE.typesetter:pushState()
   SILE.settings.temporarily(function ()
     SILE.settings.set("current.parindent", SILE.length())
     SILE.settings.set("document.parindent", SILE.length())
     for _, entry in ipairs(data) do
-      if #entry > ncols then
-        ncols = #entry
+      if #entry > numberOfCols then
+        numberOfCols = #entry
       end
       for i = 2, #entry do
         local v = tonumber(entry[i]) or 0
@@ -191,7 +193,38 @@ local function measureBiggestValueDimens (data)
     end
     SILE.typesetter:popState()
   end)
-  return maxWidth, maxHeight, minValue, maxValue, ncols
+  return maxWidth, maxHeight, minValue, maxValue, numberOfCols
+end
+
+-- DATA READING
+
+-- CSV support is straight-forward :)
+local function readCsvFile (file)
+  local data, err = pl.data.read(file, {
+    csv = true,
+    no_convert = true,
+  })
+  if not data then
+    SU.error("Failure to open CSV file " .. file .. " (" .. err .. ")")
+  end
+
+  local numberOfBars = #data
+  local maxValueWidth, maxValueHeight, minValue, maxValue, numberOfCols = measureBiggestValueDimens(data)
+  local maxCatWidth, maxCatHeight = measureBiggestCategoryDimens(data)
+  local maxHeadWidth, maxHeadHeight = measureBiggestHeadersDimens(data)
+  local props = {
+    numberOfBars = numberOfBars,
+    numberOfCols = numberOfCols,
+    maxCatWidth = maxCatWidth,
+    maxCatHeight = maxCatHeight,
+    maxHeadWidth = maxHeadWidth,
+    maxHeadHeight = maxHeadHeight,
+    maxValueWidth = maxValueWidth,
+    maxValueHeight = maxValueHeight,
+    minValue = minValue,
+    maxValue = maxValue,
+  }
+  return data, props
 end
 
 -- TEXT SCALING TO SIZE
@@ -202,62 +235,99 @@ end
 --   e.g. below a certain ratio (e.g. 0.6 would seem neat: in a 10pt
 --   document, anything below 6pt gets too small to read)
 local function textToScaledBox(text, width, dimenW, dimenH, angle)
-  local fontOptions = SILE.font.loadDefaults({})
-  local ratio = width.length:tonumber() / dimenW.length:tonumber()
-  ratio = SU.min(ratio,1)
-  if ratio > 0.0 then -- FIXME when rotation works: around 0.6
-    -- E.g. for a 10pt font, start rotating below 6pt
-    SILE.call("font", {size = fontOptions.size * ratio}, function()
-      SILE.call("strut", { method = "rule"})
-      SILE.process({ text })
-    end)
-  else
-    ratio = width.length:tonumber() / dimenH.length:tonumber()
+  return SILE.call("hbox", {}, function ()
+    local fontOptions = SILE.font.loadDefaults({})
+    local ratio = width.length:tonumber() / dimenW.length:tonumber()
+    -- print("ration", text, width.length:tonumber(), dimenW.length:tonumber(), ratio)
     ratio = SU.min(ratio, 0.9)
-    SILE.call("font", {size = fontOptions.size * ratio}, function()
-      SILE.call("strut", { method = "rule"})
-      -- FIXME
-      -- Rotate doesn't do what I quite wanted, but I am too bad at angles
-      -- to understand what it does and how to fix it.
-      SILE.call("rotate", { angle = angle }, function ()
-        SILE.process({ text })
+    if ratio > 0.0 then -- FIXME when rotation works: around 0.6
+      -- E.g. for a 10pt font, start rotating below 6pt
+      SILE.call("font", { size = fontOptions.size * ratio }, function ()
+        SILE.call("strut", { method = "character" })
+        SILE.process({text})
       end)
-    end)
-  end
+    else
+      ratio = width.length:tonumber() / dimenH.length:tonumber()
+      ratio = SU.min(ratio, 0.9)
+      SILE.call("font", { size = fontOptions.size * ratio }, function ()
+        SILE.call("strut", { method = "character" })
+        -- FIXME
+        -- Rotate doesn't do what I quite wanted, but I am too bad at angles
+        -- to understand what it does and how to fix it.
+        SILE.call("rotate", { angle = angle }, function()
+          SILE.process({text})
+        end)
+      end)
+    end
+  end)
 end
 
 -- GRAPH BAR CONSTRUCTION
 
 -- ******************* EXPERIMENTAL EXPERIMENTAL EXPERIMENTAL *******************************
-SILE.registerCommand("hbar", function (options, _)
-  local value = tostring(options.value)
-  local key = tostring(options.key)
-  
-  local rough = SU.boolean(options.rough, false)
-  local maxW, maxH = options.maxW, options.maxH
-  local maxCatW, maxCatH = options.maxCatW, options.maxCatH
+SILE.require("packages/rules")
 
+local function bargraphCategoryBlock(category, props, func)
+  local graphBox, categoryBox
+  local separator = SILE.length("1pt") -- Could be a setting or options?
+
+  SILE.typesetter:pushState()
+  SILE.settings.temporarily(function()
+    SILE.settings.set("current.parindent", SILE.length())
+    SILE.settings.set("document.parindent", SILE.length())
+    graphBox = SILE.call("hbox", {}, func)
+    categoryBox = textToScaledBox(category, 0.75 * graphBox.width:absolute(), props.maxCatWidth, props.maxCatHeight, 0)
+  end)
+  SILE.typesetter:popState()
+
+  -- Stack the categoryBox below the graph bars
+  local adjustedDepth = graphBox.depth:absolute() + categoryBox.height:absolute() + categoryBox.depth:absolute() +
+                            separator:absolute()
+  SILE.typesetter:pushHbox({
+    width = graphBox.width:absolute(),
+    height = graphBox.height:absolute(),
+    depth = adjustedDepth,
+    outputYourself = function(self, typesetter, line)
+      local outputWidth = SU.rationWidth(self.width, self.width, line.ratio)
+      local saveX = typesetter.frame.state.cursorX
+      local saveY = typesetter.frame.state.cursorY
+
+      -- Graph box
+      graphBox:outputYourself(typesetter, { ratio = 1 })
+
+      -- Category box
+      typesetter.frame.state.cursorX = saveX
+      typesetter.frame.state.cursorY = saveY
+      typesetter.frame:advancePageDirection(graphBox.depth + categoryBox.height + separator)
+      typesetter.frame:advanceWritingDirection((self.width - categoryBox.width) / 2)
+      categoryBox:outputYourself(typesetter, { ratio = 1 })
+
+      typesetter.frame.state.cursorX = saveX
+      typesetter.frame.state.cursorY = saveY
+      typesetter.frame:advanceWritingDirection(outputWidth)
+    end
+  })
+end
+
+
+local function bargraphValue(field, value, props, options)
+  local rough = SU.boolean(options.rough, false)
+  options.borderwidth = SU.cast("measurement", options.borderwidth or "0.3pt"):tonumber()
+
+  local separator = SILE.length("1pt") -- Could be a setting or options?
 
   local width = SU.cast("length", options.width)
   local height = SU.cast("length", options.height)
   local depth = SU.cast("length", options.depth)
-  local painter = PathRenderer(rough and RoughPainter() or nil)
-  local borderwidth = SU.cast("measurement", options.borderwidth or "0.3pt"):tonumber()
-  local bordercolor = options.bordercolor
-  local fillcolor = options.fillcolor
 
-  SILE.call("hbox", {}, function ()
-    SILE.settings.temporarily(function ()
-      textToScaledBox(value, 0.9 * width, maxW, maxH, -90)
-    end)
-  end)
-  local hValueBox = table.remove(SILE.typesetter.state.nodes) -- steal it back
-  SILE.call("hbox", {}, function ()
-    SILE.settings.temporarily(function ()
-      textToScaledBox(key, width, maxCatW, maxCatH, 90)
-    end)
-  end)
-  local hKeyBox = table.remove(SILE.typesetter.state.nodes) -- steal it back
+  local painter = PathRenderer(rough and RoughPainter() or nil)
+
+  local hValueBox, hKeyBox
+  SILE.typesetter:pushState()
+  hValueBox = textToScaledBox(tostring(value), 0.8 * width, props.maxValueWidth, props.maxValueHeight, -90)
+  hKeyBox = textToScaledBox(field, width, options.maxCatW, options.maxCatH, 90)
+  SILE.typesetter:popState()
+
   if tonumber(value) < 0 then
     -- Swap value and key for negative values.
     local tmp = hKeyBox
@@ -265,39 +335,40 @@ SILE.registerCommand("hbar", function (options, _)
     hValueBox = tmp
   end
 
+  hValueBox.depth = hValueBox.depth + separator:absolute()
+  hKeyBox.height = hKeyBox.height + separator:absolute()
+
   local boxH = hValueBox.height + hValueBox.depth
   local boxD = hKeyBox.height + hKeyBox.depth
-  
 
   SILE.typesetter:pushHbox({
     width = width:absolute(),
     height = height:absolute() + boxH,
     depth = depth:absolute() + boxD,
     value = hValueBox,
-    outputYourself= function (self, typesetter, line)
+    outputYourself = function(self, typesetter, line)
       local outputWidth = SU.rationWidth(self.width, self.width, line.ratio)
 
       -- Value
       local saveX = typesetter.frame.state.cursorX
       local saveY = typesetter.frame.state.cursorY
-      typesetter.frame:advanceWritingDirection((self.width - hValueBox.width) /  2)
+      typesetter.frame:advanceWritingDirection((self.width - hValueBox.width) / 2)
       typesetter.frame:advancePageDirection(-self.height + hValueBox.height)
-      
-      self.value:outputYourself(typesetter, {ratio = 1})
+
+      self.value:outputYourself(typesetter, { ratio = 1 })
       typesetter.frame.state.cursorX = saveX
       typesetter.frame.state.cursorY = saveY
-      --
 
       -- Key
-      typesetter.frame:advanceWritingDirection((self.width - hKeyBox.width) /  2)
+      typesetter.frame:advanceWritingDirection((self.width - hKeyBox.width) / 2)
       typesetter.frame:advancePageDirection(self.depth - boxD + hKeyBox.height)
-      
-      hKeyBox:outputYourself(typesetter, {ratio = 1})
+
+      hKeyBox:outputYourself(typesetter, { ratio = 1 })
       typesetter.frame.state.cursorX = saveX
       typesetter.frame.state.cursorY = saveY
-      --
-     
-      typesetter.frame:advancePageDirection(-self.height+boxH)
+
+      -- Graph bar
+      typesetter.frame:advancePageDirection(-self.height + boxH)
       local oldx = typesetter.frame.state.cursorX
       local oldy = typesetter.frame.state.cursorY
       typesetter.frame:advanceWritingDirection(outputWidth)
@@ -305,124 +376,123 @@ SILE.registerCommand("hbar", function (options, _)
       local newx = typesetter.frame.state.cursorX
       local newy = typesetter.frame.state.cursorY
 
-      local path = painter:rectangle(0, 0, (newx - oldx):tonumber() , (newy - oldy):tonumber(), {
-        fill = fillcolor, stroke = bordercolor, strokeWidth = borderwidth,
-        preserveVertices = true,
+      local path = painter:rectangle(0, 0, (newx - oldx):tonumber(), (newy - oldy):tonumber(), {
+        fill = options.fillcolor,
+        stroke = options.bordercolor,
+        strokeWidth = options.borderwidth,
+        preserveVertices = true
         -- disableMultiStroke = true,
       })
-      SILE.outputter:drawSVG(path, oldx, oldy, newx - oldx, 0, 1)   
+      SILE.outputter:drawSVG(path, oldx, oldy, newx - oldx, 0, 1)
 
       typesetter.frame:advancePageDirection(-self.depth + boxD)
     end
   })
-end, "I am ugly to do it this way")  
-
-local function readCsvFile (file)
-  local data, err = pl.data.read(file, { csv = true, no_convert = true })
-  if not data then
-    SU.error("Failure to open CSV file "..file.." (" .. err .. ")")
-  end
-  return data
 end
 
-SILE.registerCommand("charts:bargraph:internal", function (options, content)
+SILE.registerCommand("charts:bargraph:internal", function(options, _)
   local csvfile = SU.required(options, "csvfile", "bargraph")
-  local data = readCsvFile(csvfile)
-  local nbars = #data
+  local data, props = readCsvFile(csvfile)
 
-  local maxW, maxH, minValue, maxValue, ncols = measureBiggestValueDimens(data)
-  local maxCatW, maxCatH = measureBiggestCategoryDimens(data)
-  local maxHeadW, maxHeadH = measureBiggestHeadersDimens(data)
-
-  local fill = SILE.colorparser(options.xxxfillcolor or "#4880bf") -- FIXME coloring...
-  local h, s, l = colorToHsl(fill)
-
-
-  local barWidth = SILE.length("0.7em"):absolute() 
-  local graphWidth = nbars * (ncols - 1) * barWidth
+  local barWidth = SU.cast("length", options.barwidth or "0.7em"):absolute()
+  -- local graphWidth = props.numberOfBars * (props.numberOfCols - 1) * barWidth
   local graphHeight = SILE.length("4em") -- graphWidth / 2
 
   -- Sorting and coloring
   local colorFn
   if not options.sort or options.sort == "none" then
     -- no sorting
-    colorFn = function(h, s, l, index)
+    colorFn = function(h, s, l, _)
       return hslToRgb(h, s, l)
     end
   elseif options.sort == "ascending" then
-    table.sort(data, function (a, b)
-      return (tonumber(a[ncols]) or 0) < (tonumber(b[ncols]) or 0)
+    table.sort(data, function(a, b)
+      return (tonumber(a[props.numberOfCols]) or 0) < (tonumber(b[props.numberOfCols]) or 0)
     end)
     colorFn = function(h, s, l, index)
-      local cscale = (1.0 - l) / nbars
-      return hslToRgb(h, s, l + cscale * (nbars - index))
+      local cscale = (1.0 - l) / props.numberOfBars
+      return hslToRgb(h, s, l + cscale * (props.numberOfBars - index))
     end
   elseif options.sort == "descending" then
-    table.sort(data, function (a, b)
-      return (tonumber(a[ncols]) or 0) > (tonumber(b[ncols]) or 0)
+    table.sort(data, function(a, b)
+      return (tonumber(a[props.numberOfCols]) or 0) > (tonumber(b[props.numberOfCols]) or 0)
     end)
     colorFn = function(h, s, l, index)
-      local cscale = (1.0 - l) / nbars
+      local cscale = (1.0 - l) / props.numberOfBars
       return hslToRgb(h, s, l + cscale * (index - 1))
     end
   else
-    SU.error("Invalid sort option '"..options.sort.."' for bargraph")
+    SU.error("Invalid sort option '" .. options.sort .. "' for bargraph")
   end
 
-  local delta = maxValue - minValue
-  -- OOPS if delta == 0
+  props.valueMaxRange = props.maxValue - props.minValue
+  if props.valueMaxRange == 0 then
+    SU.error("Data value range for graph is null")
+  end
 
   local bordercolor = SILE.colorparser(options.bordercolor or "#a0a0a0")
-  
 
-  local barInnerSep = (ncols > 2 and 0.15 or 0) * barWidth
-  local barOutterSep = (ncols > 2 and 0.5 or 0.15) * barWidth
+  local barInnerSep = (props.numberOfCols > 2 and 0.15 or 0) * barWidth
+  local barOutterSep = (props.numberOfCols > 2 and 0.5 or 0.15) * barWidth
 
-
-  for k, v in ipairs(data) do
-    for iv = 2, ncols do
-      local hh = (iv - 1) / (#v + 1)
-      local fillcolor = colorFn(hh, s, l, k)
-      --bordercolor =  hslToRgb(hh, s, l)
-      local vx = (tonumber(v[iv]) or 0) / math.abs(delta) * graphHeight:tonumber()
-      local key
-      if ncols < 3 then
-        key = v[1]
-      else
-        key = data.original_fieldnames[iv]
-        maxCatH = maxHeadH
-        maxCatW = maxHeadW
-      end
-      SILE.call("hbar", { rough = options.rough,
-        height = vx > 0 and vx or 0,
-        depth = vx < 0 and -vx or 0, width = barWidth, 
-        sep = 0.15 * barWidth,
-        maxH = maxH, maxW = maxW,
-        maxCatH = maxCatH, maxCatW = maxCatW,
-        fillcolor = fillcolor, bordercolor = bordercolor, value = tonumber(v[iv]) or 0,
-          key =  key
-        })
-        if iv ~= ncols then SILE.call("kern", { width = barInnerSep }) end 
+  -- FIXME Ugly PoC... This needs a good refactoring!!!
+  local block = props.numberOfCols > 2 and bargraphCategoryBlock or function(_,_,fn) fn() end
+  for row, v in ipairs(data) do
+    SILE.call("hbox", {
+      padding = 0
+    }, function()
+      block(v[1], props, function()
+        SILE.call("hbox", {
+          padding = 0
+        }, function()
+          for col = 2, props.numberOfCols do
+            local hue = (col - 1) / (props.numberOfCols + 1)
+            local fillcolor = colorFn(hue, 0.4, 0.5, row)
+            local value = tonumber(v[col]) or 0
+            local barHeight = value / math.abs(props.valueMaxRange) * graphHeight:tonumber()
+            local key
+            local maxKeyHeight, maxKeyWidth
+            if props.numberOfCols < 3 then
+              key = v[1]
+              maxKeyHeight = props.maxCatHeight
+              maxKeyWidth = props.maxCatWidth
+            else
+              key = data.original_fieldnames[col]
+              maxKeyHeight = props.maxHeadHeight
+              maxKeyWidth = props.maxHeadWidth
+            end
+            bargraphValue(key, value, props, {
+              rough = options.rough,
+              height = barHeight > 0 and barHeight or 0,
+              depth = barHeight < 0 and -barHeight or 0,
+              width = barWidth,
+              maxCatH = maxKeyHeight,
+              maxCatW = maxKeyWidth,
+              fillcolor = fillcolor,
+              bordercolor = bordercolor,
+            })
+            if col ~= props.numberOfCols then
+              SILE.call("kern", { width = barInnerSep })
+            end
+          end
+        end)
+      end)
+    end)
+    if row ~= props.numberOfBars then
+      SILE.call("kern", { width = barOutterSep })
     end
-    if k ~= nbars then SILE.call("kern", { width = barOutterSep }) end 
   end
 end)
 
 local function frameWrapper (framed, rough, func)
   if framed then
     if rough then
-      SILE.call("roughbox", { padding = "1.5spc", borderwidth = 0.3 , singlestroke = true, enlarge = true, preserve = true }, function ()
-        func()
-      end)
+      SILE.call("roughbox", { padding = "1.5spc", borderwidth = 0.3, singlestroke = true, enlarge = true, preserve = true }, func)
     else
-      SILE.call("framebox", { padding = "1.5spc", borderwidth = 0.3 }, function ()
-        func()
-      end)
+      SILE.call("framebox", { padding = "1.5spc", borderwidth = 0.3 }, func)
     end
   else
-    SILE.call("hbox", {}, function ()
-      func()
-    end)
+    SILE.call("hbox", {}, func)
   end
 end
 
