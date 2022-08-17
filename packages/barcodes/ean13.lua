cc7d79b63c232f99d744f5789dbe8a8a23b1c490
Omikhleia <didier.willis@gmail.com>
2022-08-17 10:44:19 +0200

    fix(packages): Better placement of textual representation in EAN-13

--- packages/barcodes/ean13.lua
+++ packages/barcodes/ean13.lua
@@ -213,17 +213,34 @@ SILE.registerCommand("ean13", function (options, _)
                                               -- the same.
     if SU.boolean(options.showdigits, true) then
       -- N.B. Option showdigits undocumented (just used for testing)
-      SILE.call("font", { family = SILE.scratch.ean13.font.family, size = SILE.scratch.ean13.font.size * X }, function()
-        SILE.call("lower", { height = 8.3333 * X }, function()
+      local deltaFontWidth = (7 - SILE.scratch.ean13.font.width) * 3 -- 6 digits measuring at most 7X:
+                                                                     -- we'll distribute the extra space evenly.
+      SILE.call("font", { family = SILE.scratch.ean13.font.family, size = SILE.scratch.ean13.font.size * X }, function ()
+        SILE.call("lower", { height = 8.3333 * X }, function ()
+        -- 106X = 11X LQZ + 3X guard + 6*7X digits + 5X guard + 6*7X digits + 3X guard
+        -- So we get back to the start of the barcode.
           SILE.call("kern", { width = -106 * X })
-          local h = SILE.call("hbox", {}, { code[1] }) -- first digit, at the start of the Left Quiet Zone
+          -- First digit, at the start of the Left Quiet Zone
+          local h = SILE.call("hbox", {}, { code:sub(1,1) })
           h.width = SILE.length()
+          -- First 6-digit sequence is at 11X LQZ + 3X guard = 14X
+          -- We add a 0.5X displacement from the normal guard (ending in a bar,
+          -- while the centralbar starts with a space)
           SILE.call("kern", { width = 14.5 * X })
+          SILE.call("kern", { width = (deltaFontWidth) * X })
           h = SILE.call("hbox", {}, { code:sub(2,7) }) -- first sequence
           h.width = SILE.length()
+          -- Second 6-digit sequence is further at 6*7X digits + 5X guard = 47X
+          -- to which we remove the previous 0.5X displacement.
+          -- Substract an additional 0.5X displacement from the central guard
+          -- (a space) so as to end at 0.5X from the ending  guard (a bar),
+          -- hence 46X...
           SILE.call("kern", { width = 46 * X })
           h = SILE.call("hbox", {}, { code:sub(8,13) }) -- last sequence
           h.width = SILE.length()
+          SILE.call("kern", { width = -(deltaFontWidth) * X })
+          -- End marker is at 6*7X + 3X guard + 7X RQZ = 52X
+          -- Corrected by the above displacement, hence 52.5X
           local l = SILE.length((52.5 - SILE.scratch.ean13.font.width) * X)
           SILE.call("kern", { width = l })
           if not addon then
@@ -334,7 +351,7 @@ return {
 The \autodoc:package{barcodes.ean13} package allows to print out an EAN-13 barcode, suitable
 for an ISBN (or ISSN, etc.)
 
-The \autodoc:command{\ean13} command takes a mandatody \autodoc:parameter{code} parameter,
+The \autodoc:command{\ean13} command takes a mandatory \autodoc:parameter{code} parameter,
 the human representation of the EAN-13 code (including its final control digit).
 It checks its consistency and displays the corresponding barcode. By default, it uses the
 recommended scale for a “consumer item” (SC2, with a “module” of 0.33mm). The size
